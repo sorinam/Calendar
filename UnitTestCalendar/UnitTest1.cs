d2e7161858f82e3649b215e78a21cc81cfa576f1
Sorina Mancas <sorinam@yahoo.com>
2015-10-30 12:24:09 +0200

    add tests

---
+++ UnitTestCalendar/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+using Should;
+
+namespace UnitTestCalendar
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ApplicationShouldAddEvent()
+        {
+            DateTime date = new DateTime(2015, 11, 20);
+            string subject = "My birthday";
+
+            Events.AddEvent(date, subject);
+            Events.Calendar[0].Date.ShouldEqual(date);
+            Events.Calendar[0].Subject.ShouldEqual(subject);
+        }
+    }
+}
