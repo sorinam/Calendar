5e1a24e8ea9bbc49b84059cbd9bac391e0bc6f19
Sorina Mancas <sorinam@yahoo.com>
2015-11-05 18:06:46 +0200

    add test files for all classes

--- UnitTestCalendar/UnitTestIOFiles.cs
+++ UnitTestCalendar/UnitTestIOFiles.cs
@@ -1,96 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Calendar;
-using Should;
-using System.IO;
-using System.Collections.Generic;
 
 namespace UnitTestCalendar
 {
     [TestClass]
-    public class EventsEnumUnitTests
+    public class UnitTestIOFiles
     {
-        [TestMethod]
-        [ExpectedException(typeof(FormatException))]
-        public void ShouldNotAddEvent()
-        {
-            string date = "2015/15/20";
-            string subject = "My birthday";
-            string description = "It will be a nice day...";
-            Events newEvent = new Events();
-            newEvent.Add(date, subject, description);
-        }
-
-        [TestMethod]
-        public void ShouldAddEvent()
-        {
-            string date = "2015/11/20";
-            string subject = "My birthday";
-            string description = "It will be a nice day...";
-            Events newEvent = new Events();
-
-            newEvent.Add(date, subject, description);
-            List<Event> listofEvents = newEvent.EventsList;
-
-            listofEvents[0].Date.ShouldEqual(Convert.ToDateTime(date));
-            listofEvents[0].Subject.ShouldEqual(subject);
-            listofEvents[0].Description.ShouldEqual(description);
-        }
-
-        [TestMethod]
-        public void ShouldAddEventWithoutDescription()
-        {
-            string date = "2015/11/20";
-            string subject = "My birthday";
-            Events newEvent = new Events();
-
-            newEvent.Add(date, subject);
-            List<Event> listofEvents = newEvent.EventsList;
-
-            listofEvents[0].Date.ShouldEqual(Convert.ToDateTime(date));
-            listofEvents[0].Subject.ShouldEqual(subject);
-        }
-
-        [TestMethod]
-        public void ShouldAddMultiLineEvent()
-        {
-            string date = "2015/11/20";
-            string subject = "My birthday";
-            string description = "It will be a nice day...\n Nice party....\n....";
-            Events newEvent = new Events();
-
-            newEvent.Add(date, subject, description);
-            List<Event> listofEvents = newEvent.EventsList;
-
-            listofEvents[0].Date.ShouldEqual(Convert.ToDateTime(date));
-            listofEvents[0].Subject.ShouldEqual(subject);
-            listofEvents[0].Description.ShouldEqual(description);
-        }
-        [TestMethod]
-        public void ShouldSortEvents()
-        {
-            Events newEvent = new Events();
-
-            string date = "2015/11/20";
-            string subject = "My birthday";
-            string description = "It will be a nice day...\n Nice party....\n....";
-            newEvent.Add(date, subject, description);
-
-            date = "2015/10/20";
-            subject = "Joana's birthday";
-            newEvent.Add(date, subject);
-
-            List<Event> expectedList = new List<Event>{
-                new Event ("2015/10/20","Joana's birthday"),
-                new Event("2015/11/20", "My birthday", "It will be a nice day...\n Nice party....\n....")};
-
-            List<Event> listofEvents = newEvent.EventsList;
-            listofEvents.Sort();
-                     
-            expectedList.ShouldEqual(listofEvents);
-
-        }
-
         //[TestMethod]
         //public void ShouldDisplayOneEvent()
         //{
@@ -300,4 +215,3 @@ namespace UnitTestCalendar
         //    //}
     }
 }
-
