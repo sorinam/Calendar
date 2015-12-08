cf84f7398e55704ddab09793333121e885d77a8b
Sorina Mancas <sorinam@yahoo.com>
2015-12-08 15:59:46 +0200

    add web project

---
+++ NodejsWebApp/server.js
@@ -0,0 +1,21 @@
+﻿var http = require('http');
+
+var fs = require('fs');
+var htmlFile;
+
+fs.readFile('./Calendar.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    htmlFile = data;
+});
+
+
+var port = process.env.port || 8800;
+http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end(htmlFile);   
+}).listen(port);
+
+
+
