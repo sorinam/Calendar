cbd0b5e0af046ae343dc23ca72f1a59859c4ea40
Sorina Mancas <sorinam@yahoo.com>
2015-12-17 23:07:06 +0200

    refactorize tests from XMLFile class to don't use files

--- UnitTestCalendar/TestXMLs.cs
+++ UnitTestCalendar/TestXMLs.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Calendar;
 using System.Collections.Generic;
 using Should;
+using System.Xml;
 
 namespace UnitTestCalendar
 {
@@ -13,57 +14,87 @@ namespace UnitTestCalendar
         [TestMethod]
         public void ShouldListAllNodesFromXML()
         {
+            XmlDocument doc = InitialiazeXMLDocument();
             string xpath = "//calendar/appointment";
-            var resultString = XMLUtils.GetValuesOfXmlElementAtXpath_XmlFromFile(xpath);
             List<string> expectedString = new List<string> { "2015/12/20First itemThis is my first XML test", "2015/12/25Second itemThis is my second XML test" };
+
+            var resultString = XMLUtils.GetValuesOfAllXmlElements(xpath, doc);
+          
             resultString.ShouldEqual(expectedString);
         }
-
+            
         [TestMethod]
         public void ShouldListOneNodeFromXMLFindAnAttribute()
         {
+            XmlDocument doc = InitialiazeXMLDocument();
             string xpath = "calendar/appointment[@ID='2']";
-            var resultString = XMLUtils.GetValueOfXmlElementAtXpath_XmlFromFile(xpath);
             var expectedString = "2015/12/25Second itemThis is my second XML test";
+
+            var resultString = XMLUtils.GetValueOfXmlElement(xpath, doc);
+            
             resultString.ShouldEqual(expectedString);
         }
 
         [TestMethod]
         public void ShouldListOneValueOfNodeFromXML()
         {
+            XmlDocument doc = InitialiazeXMLDocument();
             string xpath = "calendar/appointment[@ID='2']/description";
-            var resultString = XMLUtils.GetValueOfXmlElementAtXpath_XmlFromFile(xpath);
             var expectedString = "This is my second XML test";
+
+            var resultString = XMLUtils.GetValueOfXmlElement(xpath, doc);
+
             resultString.ShouldEqual(expectedString);
         }
 
         [TestMethod]
         public void ShouldSetValueOfNodeToXML()
         {
+            XmlDocument doc = InitialiazeXMLDocument();
             string xpath = "calendar/appointment[@ID='2']/description";
             string newValue = "This is my modified second XML test";
-            var resultString = XMLUtils.SetValueOfXmlElementAtXpath_XmlFromFile(xpath, newValue);
+
+            var resultString = XMLUtils.SetValueOfXmlElement(xpath, newValue,doc);
+
             resultString.ShouldBeTrue();
         }
 
         [TestMethod]
         public void ShouldAddANewNodeToXML()
         {
+            XmlDocument doc = InitialiazeXMLDocument();
             string ID = "3";
             string data = "2015/12/18";
             string title = "@Rares #bday";
             string description = "don't forget to #call him";
-            var result = XMLUtils.AddNewAppointmentElementToXMLFile(ID, data, title, description);
+
+            var result = XMLUtils.AddNewNodeToXmlDocument(ID, data, title, description,ref doc);
+
             result.ShouldBeTrue();
+        }
+
+        [TestMethod]
+        public void ShouldAddAppointmentsToXMLFile()
+        {
+            XmlDocument doc = InitialiazeXMLDocument();
+            List<Event> eventsList = new List<Event>{
+            new Event("2015/12/27","One","One test"),
+            new Event("2015/11/25", "Two") };
+            Events newAppoinments = new Events(eventsList);
+
+            //XMLUtils.AddNewEventsToXMLFile(newAppoinments);
+
         }
 
         [TestMethod]
         public void ShouldListLastIDValueFromXML()
         {
-            //string xpath = "(calendar/appointment)[last()]";
-            //var resultString = XMLUtils.GetValueOfIDFromXMLFile(xpath);
-            var resultString = XMLUtils.GetLastIDFromXMLFile();
-            var expectedString = "3";
+            XmlDocument doc = InitialiazeXMLDocument();
+            string xpath = "(calendar/appointment)[last()]";
+            var expectedString = "2";
+
+            var resultString = XMLUtils.GetIDFromNodeAttribute(xpath,doc);
+                        
             resultString.ShouldEqual(expectedString);
         }
 
@@ -79,15 +110,12 @@ namespace UnitTestCalendar
             Utils.AssertAreEqual(XMLUtils.LoadEventsFromXMLFile(),expectedEventsList);
         }
 
-        [TestMethod]
-        public void ShouldAddAppointmentsToXMLFile()
+        private static XmlDocument InitialiazeXMLDocument()
         {
-            List<Event> eventsList = new List<Event>{
-            new Event("2015/12/27","One","One test"),
-            new Event("2015/11/25", "Two") };
-            Events newAppoinments = new Events(eventsList);
-            XMLUtils.SaveEventsToXMLFile(newAppoinments);
-           
+            XmlDocument doc = new XmlDocument();
+            string xmlData = @"<calendar><appointment ID=""1""><data>2015/12/20</data><title>First item</title><description>This is my first XML test</description></appointment><appointment ID=""2""><data>2015/12/25</data><title>Second item</title><description>This is my second XML test</description></appointment></calendar>";
+            doc.LoadXml(xmlData);
+            return doc;
         }
     }
 }
---
+++ XMLFile.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace Calendar
+{
+    public class XMLUtils
+    {
+        const string calendarXMLFile = @"calendar.xml";
+        public static string GetValueOfXmlElementAtXpath_XmlFromFile(string xpath)
+        {
+            string result = "";
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(calendarXMLFile);
+                result = GetValueOfXmlElement(xpath, xmlDocument);
+            }
+            catch (Exception e)
+            { }
+            return result;
+        }
+
+        public static string GetValueOfXmlElement(string xpath, XmlDocument xmlDocument)
+        {
+            string result = "";
+            XmlNode element = xmlDocument.SelectSingleNode(xpath);
+            if (element != null)
+            {
+                result = element.InnerText;
+            }
+            return result;
+        }
+
+        public static List<string> GetValuesOfXmlElementAtXpath_XmlFromFile(string xpath)
+        {
+            List<string> result = new List<string>();
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(calendarXMLFile);
+                result=GetValuesOfAllXmlElements(xpath, xmlDocument);
+            }
+            catch (Exception)
+            { }
+            return result;
+        }
+
+        public static List<string> GetValuesOfAllXmlElements(string xpath, XmlDocument xmlDocument)
+        {
+            List<string> result = new List<string>();
+            XmlNodeList nodeList = xmlDocument.SelectNodes(xpath);
+            if (nodeList != null)
+            {
+                foreach (XmlNode node in nodeList)
+                {
+                    result.Add(node.InnerText);
+                }
+            }
+            return result;
+        }
+
+        public static bool SetValueOfXmlElementAtXpath_XmlFromFile(string xpath, string newValue)
+        {
+            bool result = false;
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(calendarXMLFile);
+                result = SetValueOfXmlElement(xpath, newValue, xmlDocument);
+            }
+            catch (Exception)
+            { }
+            return result;
+        }
+
+        public static bool SetValueOfXmlElement(string xpath, string newValue, XmlDocument xmlDocument)
+        {
+            bool result = false;
+            XmlNode element = xmlDocument.SelectSingleNode(xpath);
+            if (element != null)
+            {
+                element.InnerText = newValue;
+                result = true;
+                xmlDocument.Save(calendarXMLFile);
+            }
+
+            return result;
+        }
+
+        public static bool AddNewEventToXMLFile(string ID, string data, string title, string description = "")
+        {
+            bool result = false;
+            if (!File.Exists(calendarXMLFile))
+            {
+                Console.WriteLine("\n\tThe source file does not exist! There are no events added to calendar!");
+                CreateEmptyXMLFile();
+            }
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(calendarXMLFile);
+                result = true;
+                AddNewNodeToXmlDocument(ID, data, title, description, ref xmlDocument);
+                xmlDocument.Save(calendarXMLFile);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n\t Exception: {0} ", e.Message);
+
+            }
+            return result;
+        }
+
+        public static bool AddNewNodeToXmlDocument(string ID, string data, string title, string description, ref XmlDocument xmlDocument)
+        {
+            bool result = false;
+            try
+            {
+                XmlElement appointmentElement = CreateNewXMLAppointment(ID, data, title, description);
+                XmlNode importNode = xmlDocument.ImportNode(appointmentElement, true);
+                xmlDocument.DocumentElement.AppendChild(importNode);
+                result = true;
+            }
+            catch { };
+            return result;
+        }
+
+        private static XmlElement CreateNewXMLAppointment(string ID, string data, string title, string description)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlElement appointmentElement = xmlDocument.CreateElement("appointment");
+            appointmentElement.SetAttribute("ID", ID);
+
+            XmlElement dataElement = xmlDocument.CreateElement("data");
+            dataElement.InnerText = data;
+            appointmentElement.AppendChild(dataElement);
+
+            XmlElement titleElement = xmlDocument.CreateElement("title");
+            titleElement.InnerText = title;
+            appointmentElement.AppendChild(titleElement);
+            if (description != "")
+            {
+                XmlElement descriptionElement = xmlDocument.CreateElement("description");
+                descriptionElement.InnerText = description;
+                appointmentElement.AppendChild(descriptionElement);
+            }
+
+            return appointmentElement;
+        }
+
+        public static bool AddNewEventsToXMLFile(Events eventsList)
+        {
+            bool result = false;
+            if (!File.Exists(calendarXMLFile))
+            {
+                Console.WriteLine("\n\tThe source file does not exist! There are no events added to calendar!");
+                CreateEmptyXMLFile();
+            }
+
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(calendarXMLFile);
+
+
+                string lastID = GetLastIDFromXMLFile();
+                var ID = Int32.Parse(lastID);
+
+                foreach (Event eventL in eventsList)
+                {
+                    ID++;
+                    AddNewNodeToXmlDocument(ID.ToString(), eventL.Date.ToString("yyyy/MM/dd").ToString(), eventL.Title, eventL.Description,ref xmlDocument);
+                    //XmlElement appointmentElement = CreateNewXMLAppointment(ID.ToString(), eventL.Date.ToString("yyyy/MM/dd").ToString(), eventL.Title, eventL.Description);
+                    //XmlNode importNode = xmlDocument.ImportNode(appointmentElement, true);
+                    //xmlDocument.DocumentElement.AppendChild(importNode);
+                }
+                xmlDocument.Save(calendarXMLFile);
+
+                Console.WriteLine("\tFile saved. Calendar contains {0} events. ", eventsList.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception : {0}  ", e.Message);
+                result = false;
+            }
+            return result;
+        }
+
+        private static void CreateEmptyXMLFile()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNode productsNode = doc.CreateElement("calendar");
+            doc.AppendChild(productsNode);
+            doc.Save(calendarXMLFile);
+        }
+        
+        public static Events LoadEventsFromXMLFile()
+        {
+            if (!File.Exists(calendarXMLFile))
+            {
+                Console.WriteLine("\n\tThe source file does not exist! There are no events added to calendar!");
+                return null;
+            }
+            else
+            {
+                return GetEventsListFromXMLFile(); ;
+            }
+
+        }
+
+        private static Events GetEventsListFromXMLFile()
+        {
+            Events list = new Events();
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(calendarXMLFile);
+                list=GetEventsListFromXmlDocument(xmlDocument);
+            }
+            catch
+            { }
+            return list;
+        }
+
+        public static Events GetEventsListFromXmlDocument(XmlDocument xmlDocument)
+        {
+            Events list = new Events();
+            var xpath = "calendar/appointment";
+            XmlNodeList nodeList = xmlDocument.SelectNodes(xpath);
+
+            if (nodeList != null)
+            {
+                foreach (XmlNode node in nodeList)
+                {
+                    string data, title, description;
+                    GetAllValuesOfNodeChilds(node, out data, out title, out description);
+                    list.Add(new Event(data, title, description));
+                }
+            }
+            return list;
+        }
+
+        public static string GetLastIDFromXMLFile()
+        {
+            string xpath = "(calendar/appointment)[last()]";
+            var ID = GetValueOfIDFromXMLFile(xpath);
+            return (ID == "") ? "0" : ID;
+        }
+        public static string GetValueOfIDFromXMLFile(string xpath)
+        {
+            string result = "";
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(calendarXMLFile);
+                result = GetIDFromNodeAttribute(xpath, xmlDocument);
+            }
+            catch
+            { }
+            return result;
+        }
+
+        public static string GetIDFromNodeAttribute(string xpath, XmlDocument xmlDocument)
+        {
+            string result = "";
+            XmlNode element = xmlDocument.SelectSingleNode(xpath);
+            if (element != null)
+            {
+                result = element.Attributes["ID"].Value;
+            }
+            return result;
+        }
+
+        private static void GetAllValuesOfNodeChilds(XmlNode node, out string data, out string title, out string description)
+        {
+            var id = node.Attributes["ID"].Value;
+            data = node["data"].InnerText;
+            title = node["title"].InnerText;
+            if (node.ChildNodes.Count == 3)
+            {
+                description = node["description"].InnerText;
+            }
+            else
+            { description = ""; }
+        }
+    }
+}
